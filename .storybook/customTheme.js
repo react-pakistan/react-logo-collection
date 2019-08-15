30f4e2e59e8579ccb180536cf70a3805d7e75227
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2019-08-15 12:44:17 +0500

    :sparkles: added bitcoin logos

--- .storybook/customTheme.js
+++ .storybook/customTheme.js
@@ -1,7 +1,7 @@
 import { create } from "@storybook/theming";
 
 export default create({
-  brandTitle: "React Commons Collection",
-  brandUrl: "https://github.com/Taimoormk/react-icon-collection",
+  brandTitle: "React Logos Collection",
+  brandUrl: "https://github.com/Taimoormk/react-logo-collection",
   brandImage: "https://i.ibb.co/fHmxTBJ/React-Pakistan-Logo.jpg",
 });
