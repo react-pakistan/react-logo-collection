17b2f32eb7ebc608fd5493896153f73dbfe80fdb
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-06-22 17:56:19 +0500

    :recycle: added generate-script script functionality

---
+++ src/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const EmojiWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const EmojiTextWrapper = styled.div`
+  font-size: 0.75em;
+  margin-top: 0.75em;
+`;
+
+export const EmojiItem = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 10em;
+  justify-content: center;
+  width: 10em;
+`;
