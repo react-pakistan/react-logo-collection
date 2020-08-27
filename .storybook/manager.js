2ea1683f475fff3f2181a412ece50408439f215a
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2020-08-27 11:29:36 +0500

    :recycle: added GA support

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,10 @@
 import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
+import ReactGA from 'react-ga';
 import customTheme from './custom-theme';
+
+ReactGA.initialize('UA-145839845-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 addons.setConfig({
   theme: {
@@ -8,5 +12,3 @@ addons.setConfig({
     ...themes.dark
   },
 });
-
-window.STORYBOOK_GA_ID = 'UA-145839845-1';
