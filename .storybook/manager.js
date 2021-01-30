35dd9239c03c8601dcfa7df20b1aa3a52df5780a
Taimoor Khan <taimoor.muhammad@arbisoft.com>
2021-01-30 08:40:04 +0500

    :recycle: upgraded dependencies

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,14 +1,10 @@
 import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
-import ReactGA from 'react-ga';
 import customTheme from './custom-theme';
-
-ReactGA.initialize('UA-145839845-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 addons.setConfig({
   theme: {
     ...customTheme,
-    ...themes.dark
+    ...themes.dark,
   },
 });
